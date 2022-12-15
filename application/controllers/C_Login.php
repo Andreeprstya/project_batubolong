988ae813554ed71283ee9cd29339e25295dc9010
Dewa Sandithama <96720142+dewasandithama@users.noreply.github.com>
2022-12-15 14:59:52 +0800

    c login

---
+++ application/controllers/C_Login.php
@@ -0,0 +1,17 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class C_Login extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+
+    }
+}
+
+/* End of file C_Login.php and path \application\controllers\C_Login.php */
