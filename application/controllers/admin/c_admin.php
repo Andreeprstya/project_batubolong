ae3307207f0fb7ad1a321b5c991a31a9830017dd
Andreeprstya <andreseptaprasetya06@gmail.com>
2022-12-28 13:19:38 +0800

    pp

--- application/controllers/admin/c_admin.php
+++ application/controllers/admin/c_admin.php
@@ -11,7 +11,6 @@ class c_admin extends CI_Controller {
     public function index()
     {
         $this->load->view('admin/header');
-        $this->load->view('admin/sidebar');
         $this->load->view('admin/body');
         $this->load->view('admin/footer');
     }
