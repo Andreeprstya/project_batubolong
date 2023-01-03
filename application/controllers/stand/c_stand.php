bf76c5c72c3d7172d0a0fbf442a2fc27d7212b9d
wahyudigilang83 <wahyudigilang83@gmail.com>
2023-01-03 09:09:13 -0800

    stand

--- application/controllers/stand/c_stand.php
+++ application/controllers/stand/c_stand.php
@@ -10,7 +10,27 @@ class c_stand extends CI_Controller {
 
     public function index()
     {
-
+        $this->load->view('layout_stand/header');
+        $this->load->view('stand/body');
+        $this->load->view('layout_stand/footer');
+    }
+    public function menu_barang()
+    {
+        $this->load->view('layout_stand/header');
+        $this->load->view('stand/menu_barang');
+        $this->load->view('layout_stand/footer');
+    }
+    public function laporan()
+    {
+        $this->load->view('layout_stand/header');
+        $this->load->view('stand/laporan');
+        $this->load->view('layout_stand/footer');
+    }
+    public function pendapatan()
+    {
+        $this->load->view('layout_stand/header');
+        $this->load->view('stand/pendapatan');
+        $this->load->view('layout_stand/footer');
     }
 }
 
