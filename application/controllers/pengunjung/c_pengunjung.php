fd8ca15d85cbd5934b56a16770584f6773dd8306
wahyudigilang83 <wahyudigilang83@gmail.com>
2023-01-11 18:25:03 -0800

    section pengunjung

--- application/controllers/pengunjung/c_pengunjung.php
+++ application/controllers/pengunjung/c_pengunjung.php
@@ -7,54 +7,84 @@ class c_pengunjung extends CI_Controller
     {
         parent::__construct();
     }
+ 
 
     public function index()
     {
-        $this->load->view('pengunjung/header');
-        $this->load->view('pengunjung/index');
-        $this->load->view('pengunjung/footer');
+        if (!isset($_SESSION['username'])) {
+            redirect('index');
+        } else {
+            $this->load->view('pengunjung/header');
+            $this->load->view('pengunjung/index');
+            $this->load->view('pengunjung/footer');
+        }
+
     }
 
     public function stand()
     {
-        $this->load->view('pengunjung/header');
-        $this->load->view('pengunjung/stand');
-        $this->load->view('pengunjung/footer');
+        if (!isset($_SESSION['username'])) {
+            redirect('index');
+        } else {
+            $this->load->view('pengunjung/header');
+            $this->load->view('pengunjung/stand');
+            $this->load->view('pengunjung/footer');
+        }
     }
 
     public function menustand()
     {
-        $this->load->view('pengunjung/header');
-        $this->load->view('pengunjung/menustand');
-        $this->load->view('pengunjung/footer');
+        if (!isset($_SESSION['username'])) {
+            redirect('index');
+        } else {
+            $this->load->view('pengunjung/header');
+            $this->load->view('pengunjung/menustand');
+            $this->load->view('pengunjung/footer');
+        }
     }
 
     public function ticket()
     {
-        $this->load->view('pengunjung/header');
-        $this->load->view('pengunjung/ticket');
-        $this->load->view('pengunjung/footer');
+        if (!isset($_SESSION['username'])) {
+            redirect('index');
+        } else {
+            $this->load->view('pengunjung/header');
+            $this->load->view('pengunjung/ticket');
+            $this->load->view('pengunjung/footer');
+        }
     }
 
     public function tunai()
     {
-        $this->load->view('pengunjung/header');
-        $this->load->view('pengunjung/tunai');
-        $this->load->view('pengunjung/footer');
+        if (!isset($_SESSION['username'])) {
+            redirect('index');
+        } else {
+            $this->load->view('pengunjung/header');
+            $this->load->view('pengunjung/tunai');
+            $this->load->view('pengunjung/footer');
+        }
     }
 
     public function nontunai()
-    {
-        $this->load->view('pengunjung/header');
-        $this->load->view('pengunjung/nontunai');
-        $this->load->view('pengunjung/footer');
+    {   
+        if (!isset($_SESSION['username'])) {
+            redirect('index');
+        } else {
+            $this->load->view('pengunjung/header');
+            $this->load->view('pengunjung/nontunai');
+            $this->load->view('pengunjung/footer');
+        }
     }
 
     public function history()
     {
-        $this->load->view('pengunjung/header');
-        $this->load->view('pengunjung/history');
-        $this->load->view('pengunjung/footer');
+        if (!isset($_SESSION['username'])) {
+            redirect('index');
+        } else {
+            $this->load->view('pengunjung/header');
+            $this->load->view('pengunjung/history');
+            $this->load->view('pengunjung/footer');
+        }
     }
 }
 
