a5e191ef2a3054fbf767b4d0d4b57dee13fc7e18
wahyudigilang83 <wahyudigilang83@gmail.com>
2023-01-11 18:03:19 -0800

    kntol

--- application/views/pengunjung/header.php
+++ application/views/pengunjung/header.php
@@ -69,6 +69,7 @@
                                                 </ul>
                                             </li>
                                             <li><a href="<?= base_url('pengunjung/c_pengunjung/history') ?>">History</a>
+                                            <li><a href="<?= base_url('auth/logout') ?>">Logout</a>
                                             </li>
                                         </ul>
                                     </nav>
