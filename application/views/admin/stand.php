a8a1a83cab3612036b07891a9ce28b07bda62145
Dewa Sandithama <96720142+dewasandithama@users.noreply.github.com>
2023-01-12 08:46:20 +0800

    data stand admin

--- application/views/admin/stand.php
+++ application/views/admin/stand.php
@@ -1,47 +1,54 @@
-    
-                <main>
-                    <div class="container-fluid px-4">
-                        <h1 class="mt-4">Data Stand Pesona Batu Bolong</h1>
-                        <!-- <ol class="breadcrumb mb-4">
+    <main>
+        <div class="container-fluid px-4">
+            <h1 class="mt-4">Data Stand Pesona Batu Bolong</h1>
+            <!-- <ol class="breadcrumb mb-4">
                             <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
                             <li class="breadcrumb-item active">Tables</li>
                         </ol> -->
-                        <div class="card mb-4">
-                            <!-- <div class="card-body">
+            <div class="card mb-4">
+                <!-- <div class="card-body">
                                 DataTables is a third party plugin that is used to generate the demo table below. For more information about DataTables, please visit the
                                 <a target="_blank" href="https://datatables.net/">official DataTables documentation</a>
                                 .
                             </div> -->
-                        </div>
-                        <div class="card mb-4">
-                            <div class="card-header">
-                                <i class="fas fa-table me-1"></i>
-                                Data Stand
-                            </div>
-                            <div class="card-body">
-                                <table id="datatablesSimple">
-                                    <thead>
-                                        <tr>
-                                            <th>First Name</th>
-                                            <th>Last Name</th>
-                                            <th>Username</th>
-                                            <th>Email</th>
-                                            <th>Sebagai</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                    <?php foreach ($stand->result_array() as $key ): ?>
-                                        <tr>
-                                            <td><?php echo $key['first_name']?></td>
-                                            <td><?php echo $key['last_name']?></td>
-                                            <td><?php echo $key['username']?></td>
-                                            <td><?php echo $key['email']?></td>
-                                            <td><?php echo $key['type']?></td>
-                                        </tr>
-                                    <?php endforeach?>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-                </main>
+            </div>
+
+            <div class="card mb-4">
+                <div class="card-header">
+                    <i class="fas fa-table me-1"></i>
+                    Data Stand
+                    <a href="<?= base_url('auth/register') ?>"><button class="btn btn-primary float-end">Tambah
+                            Stand</button></a>
+                </div>
+                <div class="card-body">
+                    <table id="datatablesSimple">
+                        <thead>
+                            <tr>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Username</th>
+                                <th>Email</th>
+                                <th>Sebagai</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($stand->result_array() as $key) : ?>
+                            <tr>
+                                <td><?php echo $key['first_name'] ?></td>
+                                <td><?php echo $key['last_name'] ?></td>
+                                <td><?php echo $key['username'] ?></td>
+                                <td><?php echo $key['email'] ?></td>
+                                <td><?php echo $key['type'] ?></td>
+                                <td>
+                                    <button class="btn btn-success floa">Edit</button>
+                                    <button class="btn btn-danger">Hapus</button>
+                                </td>
+                            </tr>
+                            <?php endforeach ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </main>
